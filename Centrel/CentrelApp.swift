56682b027f147923e99cecc9f18bfe72f88c3c48
maxweissenbaeck <max.weissenbaeck@hotmail.com>
2025-05-03 00:32:26 +0200

    Initial Commit

---
+++ Centrel/CentrelApp.swift
@@ -0,0 +1,17 @@
+//
+//  CentrelApp.swift
+//  Centrel
+//
+//  Created by Max Weißenbäck on 03.05.25.
+//
+
+import SwiftUI
+
+@main
+struct CentrelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CentrelTests/CentrelTests.swift
@@ -0,0 +1,17 @@
+//
+//  CentrelTests.swift
+//  CentrelTests
+//
+//  Created by Max Weißenbäck on 03.05.25.
+//
+
+import Testing
+@testable import Centrel
+
+struct CentrelTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
